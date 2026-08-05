d20b38ba648ec36fe85fbc868f876f18db43d34e
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-08-05 23:40:02 +0530

    feat(ticket-service): initialize ticket service with range allocation functionality

---
+++ ticket-service/src/app.ts
@@ -0,0 +1,77 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "../.env",
+  quiet: true,
+});
+
+import express from "express";
+import { eq } from "drizzle-orm";
+import type { Request, Response } from "express";
+import db from "./db/dbConnection.js";
+import allocations from "./db/schema.js";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const RANGE_SIZE = 1500000;
+
+app.use(express.json());
+
+app.get("/", function (req: Request, res: Response) {
+  res.send("Server is healthy");
+});
+
+async function allocateRange() {
+  try {
+    const ranges = await db.transaction(async (tx) => {
+      //Fetch the counter and lock that row
+      const [row] = await tx
+        .select()
+        .from(allocations)
+        .where(eq(allocations.id, 1))
+        .for("update");
+
+      if (!row) {
+        throw new Error("Row doesn't exist");
+      }
+
+      const next = Number(row.next_available);
+      const start = next;
+      const end = next + RANGE_SIZE - 1;
+
+      //update the row to point to next_available range
+      await tx
+        .update(allocations)
+        .set({
+          next_available: start + RANGE_SIZE,
+        })
+        .where(eq(allocations.id, 1));
+
+      return { start, end };
+    });
+    return ranges;
+  } catch (err) {
+    if (err instanceof Error) console.log(err.message);
+    else console.log("Unknown Error", err);
+  }
+}
+
+//use post request when u are creating/allocating resources not get or put
+app.post("/api/v1/ranges/allocate", async (req: Request, res: Response) => {
+  const ranges = await allocateRange();
+  return res.json(ranges);
+});
+
+async function bootstrap() {
+  try {
+    app.listen(PORT, () => {
+      console.log("Server is running on PORT: ", PORT);
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) console.log(err.message);
+    else console.log("Unknown Error: ", err);
+
+    process.exit(1);
+  }
+}
+
+bootstrap();
