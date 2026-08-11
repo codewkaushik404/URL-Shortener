ac07533649cd81e43996a95b156f951a136d89af
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-08-11 23:31:11 +0530

    chore: update database URL in drizzle.config and adjust tsconfig paths

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: "./src/database/schema",
   dialect: "mysql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: process.env.WRITE_DATABASE_URL!
   }
 })
